02302c3c300843d820257a33be15b220858271ce
YeanSe <1730214056@qq.com>
2022-07-15 13:25:11 +0800

    🔧eslint config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,14 @@
 /// <reference types="vitest" />
+import path from 'path'; // eslint-disable-line
 import { defineConfig } from 'vite';
-import path from 'path';// eslint-disable-line
 
 export default defineConfig({
   test: {
-    globals: false,
+    globals: true,
     environment: 'happy-dom',
     cache: {
       dir: path.resolve(__dirname, './node_modules/.vitest'),
     },
+    exclude: ['**/node_modules/**', '**/samples/**'],
   },
 });
